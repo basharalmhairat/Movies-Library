02d92032773f7f5b0d87e49b20703d2beed6ef23
basharalmhairat <basharalmhairat6y6u@gmail.com>
2022-02-03 01:38:40 +0200

    movieli

--- moviesdata/schema.sql
+++ moviesdata/schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS movies;
 CREATE TABLE IF NOT EXISTS movies (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
-    release_date INTEGER,
+    release_date VARCHAR(255),
     poster_path VARCHAR(10000),
-    overview VARCHAR(10000),
+    overview VARCHAR(10000)
 );
