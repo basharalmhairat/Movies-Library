34dbea4a323faa9fd287a54b9b06116eb6eb182e
basharalmhairat <basharalmhairat6y6u@gmail.com>
2022-02-09 11:11:34 +0200

    ff

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const pg = require('pg');
 // const client = new pg.Client(process.env.DATABASE_URL);
 const client = new pg.Client({
     connectionString: process.env.DATABASE_URL,
-    // ssl: { rejectUnauthorized: false }
+     ssl: { rejectUnauthorized: false }
 })
 
 Server.get('/', homePage);
